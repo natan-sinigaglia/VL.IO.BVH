2f318dfe13ef2b102d6aabbb754cd02904e5fa48
natan-sinigaglia <natan.sinigaglia@gmail.com>
2024-10-20 02:24:26 +0200

    wip

---
+++ csharp/VL.IO.BVH/Utils.cs
@@ -0,0 +1,16 @@
+// For examples, see:
+using System;
+using System.Collections.Generic;
+
+namespace BVH.Type
+{
+    public enum BVHChannel
+    {
+        X_POSITION,
+        Y_POSITION,
+        Z_POSITION,
+        X_ROTATION,
+        Y_ROTATION,
+        Z_ROTATION
+    }
+}
